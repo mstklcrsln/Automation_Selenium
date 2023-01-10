9757074bffcd9d26394fd6bebb36a2053133fdae
Mesut Kılıçarslan <mstklcrsln81@gmail.com>
2023-01-10 15:30:35 +0300

    day 8 utility class

--- src/test/java/com/cydeo/utilities/BrowserUtils.java
+++ src/test/java/com/cydeo/utilities/BrowserUtils.java
@@ -1,10 +1,19 @@
 package com.cydeo.utilities;
 
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+
+import java.util.Set;
+
 public class BrowserUtils {
+
+    /*
+    in this class only general utility methods that are not related to some specific page
+     */
 
 
     //This method will accept int (in seconds and execute thread.sleep for given duration
-    public void sleep(int second) {
+    public static void sleep(int second) {
         second *= 1000;
         try {
             Thread.sleep(second);
@@ -12,5 +21,21 @@ public class BrowserUtils {
         }
 
 
+    }
+
+    //switch window and verify
+
+    public static void switchWindowAndVerify(WebDriver driver, String expectedInURL, String expectedTitle) {
+
+        Set<String> allWindowsHandles = driver.getWindowHandles();
+
+        for (String each : driver.getWindowHandles()) {
+            driver.switchTo().window(each);
+            if (driver.getCurrentUrl().contains(expectedInURL)) { // it stops on the wanted page
+                break;
+            }
+        }
+        String actualTitle= driver.getTitle();
+        Assert.assertTrue(actualTitle.contains(expectedTitle));
     }
 }
